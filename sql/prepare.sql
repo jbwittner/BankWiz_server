18ffe70e41be214302f62fe64a6073a8ce4a99d9
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-10-31 21:42:15 +0100

    TODO: Fix later

--- sql/prepare.sql
+++ sql/prepare.sql
@@ -1,11 +1,25 @@
-DROP USER IF EXISTS bankwiz_user@'localhost';
-DROP USER IF EXISTS bankwiz_user@'%';
-DROP DATABASE IF EXISTS bankwiz_db;
+DROP
+    USER IF EXISTS bankwiz_user@'localhost';
 
-CREATE DATABASE bankwiz_db;
-CREATE USER bankwiz_user@'localhost' IDENTIFIED BY 'Bankwiz2024';
-CREATE USER bankwiz_user@'%' IDENTIFIED BY 'Bankwiz2024';
+DROP
+    USER IF EXISTS bankwiz_user@'%';
 
-GRANT ALL PRIVILEGES ON bankwiz_db.* TO bankwiz_user@'localhost';
-GRANT ALL PRIVILEGES ON bankwiz_db.* TO bankwiz_user@'%';
+DROP
+    DATABASE IF EXISTS bankwiz_db;
+
+CREATE
+    DATABASE bankwiz_db;
+
+CREATE
+    USER bankwiz_user@'localhost' IDENTIFIED BY 'Bankwiz2024';
+
+CREATE
+    USER bankwiz_user@'%' IDENTIFIED BY 'Bankwiz2024';
+
+GRANT ALL PRIVILEGES ON
+bankwiz_db.* TO bankwiz_user@'localhost';
+
+GRANT ALL PRIVILEGES ON
+bankwiz_db.* TO bankwiz_user@'%';
+
 FLUSH PRIVILEGES;
