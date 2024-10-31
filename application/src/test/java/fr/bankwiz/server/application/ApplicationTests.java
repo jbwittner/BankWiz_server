c8c869b0071f7a864c20741b9426a1c05c169903
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-10-31 17:51:04 +0100

    This solves it.

--- application/src/test/java/fr/bankwiz/server/application/ApplicationTests.java
+++ application/src/test/java/fr/bankwiz/server/application/ApplicationTests.java
@@ -1,12 +1,12 @@
 package fr.bankwiz.server.application;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.assertThat(true).isTrue();
+        Assertions.assertTrue(true);
     }
 }
