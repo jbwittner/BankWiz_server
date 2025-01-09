bb1cd9fccd3fc401822c49a097daabe5bf6f6cb9
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2025-01-09 18:00:40 +0100

    Herping the fucking derp right here and now.

--- infrastructure/api-rest/src/main/java/fr/bankwiz/server/infrastructure/apirest/configuration/SecurityConfiguration.java
+++ infrastructure/api-rest/src/main/java/fr/bankwiz/server/infrastructure/apirest/configuration/SecurityConfiguration.java
@@ -20,9 +20,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Value("${application.web.url}")
-    private String applicationUrl;
-
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         final JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
         converter.setAuthoritiesClaimName("permissions");
@@ -36,8 +33,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http, final WebProperties webProperties)
             throws Exception {
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource(webProperties)))
-                .authorizeHttpRequests(authorize -> authorize
+
+        if(webProperties.isEnableCors()){
+            http.cors(cors -> cors.configurationSource(corsConfigurationSource(webProperties)));
+        }
+
+        http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
                                 "/status/public",
                                 "/v3/api-docs/**",
@@ -60,7 +61,6 @@ public class SecurityConfiguration {
 
         final CorsConfiguration configuration = new CorsConfiguration();
         final List<String> allowedOriginList = new ArrayList<>(webProperties.getCorsAllowedOrigins());
-        allowedOriginList.add(applicationUrl);
         configuration.setAllowedOrigins(allowedOriginList);
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
